59b61719c2c2580339af5ba1446d22603bc2ba06
Irina <Irina>
2022-09-15 15:40:45 +0300

    environment setting

--- conftest.py
+++ conftest.py
@@ -1,11 +1,12 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.options import Options as OC
+from selenium.webdriver.firefox.options import Options as OF
 
 def pytest_addoption(parser):
-    parser.addoption('--browser_name', action='store', default=None, #default="chrome"
+    parser.addoption('--browser_name', action='store', default='chrome', 
                      help="Choose browser: chrome or firefox")
-    parser.addoption('--language', action='store', default=None, #default="ru"
+    parser.addoption('--language', action='store', default='en',
                      help="Choose language: ru/en/fr...")
 
 
@@ -17,19 +18,18 @@ def browser(request):
     browser = None
     if browser_name == "chrome":
         print("\nstart chrome browser for test..")
-        options = Options()
+        options = OC()
         options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
         browser = webdriver.Chrome(options=options)
     elif browser_name == "firefox":
         print("\nstart firefox browser for test..")
-        fp = webdriver.FirefoxProfile()
-        fp.set_preference("intl.accept_languages", user_language)
-        browser = webdriver.Firefox(firefox_profile=fp)
+        options = OF()
+        options.set_preference("intl.accept_languages", user_language)
+        browser = webdriver.Firefox(options=options)
     else:
         raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser
     print("\nquit browser..")
     browser.quit()
     
-    # Теперь, сколько бы файлов с тестами мы ни создали, у тестов будет доступ к фикстуре browser. Фикстура передается в тестовый метод в качестве аргумента. Таким образом можно удобно переиспользовать одни и те же вспомогательные функции в разных частях проекта.
     
